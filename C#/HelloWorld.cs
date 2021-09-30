3827c242ca057b3e1eeafe0f6024ab1c47152110
knchapagai <91677785+knchapagai@users.noreply.github.com>
2021-09-30 23:02:26 +0545

    Create HelloWorld.cs

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,10 @@
+/* Program to print string `Hello, World! in C#` */
+namespace HelloWorld
+{
+    class Hello {        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
