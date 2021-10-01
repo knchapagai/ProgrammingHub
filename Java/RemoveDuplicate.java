5f255e2f810d41f0f1e51e07874f0bece09c533a
knchapagai <91677785+knchapagai@users.noreply.github.com>
2021-10-01 22:02:28 +0545

    Formatted the code

--- Java/RemoveDuplicate.java
+++ Java/RemoveDuplicate.java
@@ -1,66 +1,24 @@
-public class RemoveDuplicate{
-  
-  static Node first;
-  static class Node{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    int data;
-    Node next;
+public class RegexMatches {
+    public static void main(String args[]) {
 
-    Node(int data){
-      this.data = data;
-      this.next = next;
+        // String to be scanned to find the pattern.
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(.*)(\\d+)(.*)";
 
+        // Create a Pattern object
+        Pattern r = Pattern.compile(pattern);
+
+        // Now create matcher object.
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+            System.out.println("Found value: " + m.group(1));
+            System.out.println("Found value: " + m.group(2));
+        } else {
+            System.out.println("NO MATCH");
+        }
     }
-
-  }
-
-  static void create(int a[]){
-    Node last,t;
-    first = new Node(a[0]);
-
-    last = first;
-
-    for(int i = 1; i <a.length; i++){
-      t = new Node(a[i]);
-
-      last.next = t;
-      last = t;
-    }
-  }
-
-   static void display(Node n){
-    if(n!=null){
-      System.out.print(n.data+" ");
-      display(n.next);
-    }
-  }
-
-  static void remove_duplicate(Node n){
-    
-    Node q = n.next;
-    while(q!=null){
-      if(n.data!=q.data){
-        n = q;
-        q = q.next;
-      }
-      else{
-        n.next = q.next;
-
-        q = n.next;
-      }
-    }
-  }
-
-  
-  
-
-  public static void main(String[] args) {
-    int a[] = {10,20,20,20,50};
-
-    create(a);
-
-    remove_duplicate(first);
-    display(first);
-  }
 }
-
