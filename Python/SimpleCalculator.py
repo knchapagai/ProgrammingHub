8bc774afaed27295a70802c96b86431ebf999176
Md. Tahmid Hossain <55309419+tahmid02016@users.noreply.github.com>
2021-10-01 10:31:11 +0600

    Create SimpleCalculator.py

---
+++ Python/SimpleCalculator.py
@@ -0,0 +1,6 @@
+# Simple calculator
+a,  b = map(int, input().split())
+print("{} + {} = {}".format(a, b, a+b))
+print("{} - {} = {}".format(a, b, a-b))
+print("{} * {} = {}".format(a, b, a*b))
+print("{} / {} = {}".format(a, b, a/b))
