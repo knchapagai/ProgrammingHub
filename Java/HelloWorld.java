d24b21c2d4c20de12dad791cd500ef62b4cad502
YoshKavi <73147391+YoshKavi@users.noreply.github.com>
2021-10-01 09:36:36 +0530

    Java Program

---
+++ Java/HelloWorld.java
@@ -0,0 +1,8 @@
+// Program to print string `Hello, World! in Java
+import java.io.*;
+ 
+class HelloWorld {
+    public static void main (String[] args) {
+       System.out.println("Hello, World!");
+    }
+}
