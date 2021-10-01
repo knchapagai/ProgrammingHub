79039e5a9427d9f6e5d684bb0e64f10e34ac5a55
TheNetHunter <91675823+TheNetHunter@users.noreply.github.com>
2021-10-01 21:37:45 +0545

    Create AddSubMulDiv.js

---
+++ JavaScript/AddSubMulDiv.js
@@ -0,0 +1,9 @@
+ var numOne=12, numTwo=10, res;
+  res = numOne + numTwo;
+  console.log("Add = " + res);
+  res = numOne - numTwo;
+  console.log("Subtract = " + res);
+  res = numOne * numTwo;
+  console.log("Multiply = " + res);
+  res = numOne/numTwo;
+  console.log("Divide = " + res);
