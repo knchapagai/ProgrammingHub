12eb805862115c21e7cba166d481d2035d64d173
Nikhil Kadam <70500923+Nick-h4ck3r@users.noreply.github.com>
2021-10-05 20:30:41 +0530

    Update and rename passwordmaker to passwordmaker.py

--- Python/passwordmaker.py
+++ Python/passwordmaker.py
@@ -1,15 +1,20 @@
+# importing libraries
 import random
 import string 
+
+# print welcome message
 print('Welcome to Password Maker!')
 adjectives = ['sleepy', 'slow', 'smelly', 'wet', 'fat', 'red','orange', 'yellow', 'green','blue', 'purple', 'fluffy','white', 'proud', 'brave']
 nouns = ['apple', 'dinosaur', 'ball','toaster', 'goat', 'dragon','hammer', 'duck', 'panda', 'cat','wolf']
+
+# main function
 while True:
     adjective = random.choice(adjectives)
     noun = random.choice(nouns)
     number = random.randrange(0, 100)
     special_char = random.choice(string.punctuation)
-    password = adjective + noun + str(number) + special_char 
-    print('Your new password is: %s' % password)
+    password = adjective + noun + special_char + str(number)
+    print(f"Your new password is: {password}")
 
     option = input('Would you like another password? Type y or n: ') 
     if option =='n':
