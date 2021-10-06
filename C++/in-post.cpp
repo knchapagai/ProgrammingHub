2655321401e503034c7d0e54b90a70f2344d82a9
Midha Shrey <midhashrey@gmail.com>
2021-10-06 22:16:21 +0530

    Added traversal codes

---
+++ C++/in-post.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int prec(char c) 
+{ 
+	if(c == '^') 
+	return 3; 
+	else if(c == '*' || c == '/') 
+	return 2; 
+	else if(c == '+' || c == '-') 
+	return 1; 
+	else
+	return -1; 
+}  
+void infixToPostfix(string s) 
+{ 
+	stack<char> st; 
+	st.push('N'); 
+	int l = s.length(); 
+	string ns; 
+	for(int i = 0; i < l; i++) 
+	{ 
+		if((s[i] >= 'a' && s[i] <= 'z')||(s[i] >= 'A' && s[i] <= 'Z')) 
+		ns+=s[i]; 
+		else if(s[i] == '(') 
+		st.push('('); 
+		else if(s[i] == ')') 
+		{ 
+			while(st.top() != 'N' && st.top() != '(') 
+			{ 
+				char c = st.top(); 
+				st.pop(); 
+			ns += c; 
+			} 
+			if(st.top() == '(') 
+			{ 
+				char c = st.top(); 
+				st.pop(); 
+			} 
+		} 
+		else{ 
+			while(st.top() != 'N' && prec(s[i]) <= prec(st.top())) 
+			{ 
+				char c = st.top(); 
+				st.pop(); 
+				ns += c; 
+			} 
+			st.push(s[i]); 
+		} 
+
+	} 
+	while(st.top() != 'N') 
+	{ 
+		char c = st.top(); 
+		st.pop(); 
+		ns += c; 
+	} 
+	
+	cout << ns << endl; 
+
+} 
+int main() 
+{ 
+	string exp; 
+    cout<<"Enter the Infix String : \n";
+    cin>>exp;
+	cout<<"The PostFix of the Inputed String is : \n";
+    infixToPostfix(exp); 
+	return 0; 
+}
