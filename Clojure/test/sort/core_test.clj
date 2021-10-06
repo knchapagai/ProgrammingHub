d8257d47f160afbe555d089aa1f452b4d0111931
Tobias Schröder <5970416+tbsschroeder@users.noreply.github.com>
2021-10-06 10:18:52 +0200

    Update core_test.clj
    Be more restrictive on imports

--- Clojure/test/sort/core_test.clj
+++ Clojure/test/sort/core_test.clj
@@ -1,13 +1,13 @@
 (ns sort.core-test
-  (:require [clojure.test :refer :all]
-            [sort.core :refer :all]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [sort.core :refer [quick-sort]]))
 
 (deftest a-test
   (testing "Woohooo"
-    (is (= nil (quick-sort []))
-    (is (= nil (quick-sort []))
-    (is (= [0] (quick-sort [0]))
+    (is (= nil (quick-sort [])))
+    (is (= nil (quick-sort [])))
+    (is (= [0] (quick-sort [0])))
     (is (= [0 1 2] (quick-sort [0 1 2])))
     (is (= [0 1 2] (quick-sort [2 1 0])))
     (is (= [1 2 8] (quick-sort [2 8 1])))
-    (is (= [-5 0 9] (quick-sort [0 -5 9]))))))
+    (is (= [-5 0 9] (quick-sort [0 -5 9])))))
