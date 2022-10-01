7b4999d8553d62d4a55682d38c31f39103232f6e
Aniket Bindhani <aniketbindhani44@gmail.com>
2022-10-01 20:19:49 +0530

    SumElementsArray.php added

---
+++ PHP/SumElementsArray.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php    
+$numbers=array("100","160","20","67");    
+echo "Sum of array elements is: ";
+echo array_sum($numbers);
+?>    
+ 
+</body>
+</html>
