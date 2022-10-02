36cf0330a5aab55993624889d159b8a8efdb800e
ThePrekshyaGyawali <91814009+ThePrekshyaGyawali@users.noreply.github.com>
2022-10-02 11:15:13 +0545

    Create Operators.dart

---
+++ Dart/Operators.dart
@@ -0,0 +1,80 @@
+void main() { 
+   var num1 = 101; 
+   var num2 = 2; 
+   var res = 0; 
+   
+   res = num1+num2; 
+   print("Addition: ${res}"); 
+   
+   res = num1-num2;
+   print("Subtraction: ${res}"); 
+   
+   res = num1*num2; 
+   print("Multiplication: ${res}"); 
+   
+   res = num1/num2; 
+   print("Division: ${res}"); 
+   
+   res = num1~/num2; 
+   print("Division returning Integer: ${res}"); 
+   
+   res = num1%num2; 
+   print("Remainder: ${res}"); 
+   
+   num1++; 
+   print("Increment: ${num1}"); 
+   
+   num2--; 
+   print("Decrement: ${num2}"); 
+}
+It will produce the following output −
+
+Addition:103 
+Subtraction: 99                               
+Multiplication: 202               
+Division: 50.5
+Division returning Integer: 50                  
+Remainder: 1      
+Increment: 102    
+Decrement: 1
+ Previous Page
+Next Page  
+Advertisements
+
+Tutorials Point
+ About us
+ Refund Policy
+ Terms of use
+ Privacy Policy
+ FAQ's
+ Contact
+© Copyright 2022. All Rights Reserved.
+void main() { 
+   var num1 = 101; 
+   var num2 = 2; 
+   var res = 0; 
+   
+   res = num1+num2; 
+   print("Addition: ${res}"); 
+   
+   res = num1-num2;
+   print("Subtraction: ${res}"); 
+   
+   res = num1*num2; 
+   print("Multiplication: ${res}"); 
+   
+   res = num1/num2; 
+   print("Division: ${res}"); 
+   
+   res = num1~/num2; 
+   print("Division returning Integer: ${res}"); 
+   
+   res = num1%num2; 
+   print("Remainder: ${res}"); 
+   
+   num1++; 
+   print("Increment: ${num1}"); 
+   
+   num2--; 
+   print("Decrement: ${num2}"); 
+}
