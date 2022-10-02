1d62561a39f666e542bb0d9e11812410817bf010
iyamnabeen <imnabeen01@gmail.com>
2022-10-02 14:54:18 +0545

    add: colors in pingg script

--- Bash/pingg.sh
+++ Bash/pingg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -q --spider http://google.com
-[ "$?" -eq 0 ] && echo -e "\e[36m   Net is on"|| echo " Net is off"
+[ "$?" -eq 0 ] && echo -e "\e[36m   Net is on" || echo -e "\e[37m  Net is off"
