8aae80501d820fef16f68b715319c23c9363dccd
Big <Big@Coralines-MacBook-Air.local>
2022-10-06 19:34:40 +0700

    add input number range

--- Python/Generate_Random_Number_List.py
+++ Python/Generate_Random_Number_List.py
@@ -1,4 +1,8 @@
 import random
 #Generate 5 random numbers between 10 and 30
+
+first=input("Enter your first number: ").strip()
+second=input("Enter your sencond number: ").strip()
+
 randomlist = random.sample(range(10, 30), 5)
 print(randomlist)
